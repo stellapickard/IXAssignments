4dd2ad80b45665e45243d1a0299e1547daa43abe
Stella <stella@pickard.co.za>
2016-06-07 13:55:58 +0200

    Day 7 morning submission

---
+++ Day_7/day7morning.js
@@ -0,0 +1,30 @@
+// window.onload = function() {
+	// console.log the values of First Name and Last Name 
+	
+	
+	function getFormValues() {  
+		var firstname = document.getElementById("firstname").value; 
+		var lastname = document.getElementById("lastname").value; 
+
+		console.log(firstname);
+		console.log(lastname);
+	}
+
+	
+
+	
+	
+
+	// Change the color of the div with id "color-div"
+	
+	function changeColor() {
+		document.getElementById("color-div").style.color = "red"; 
+	}
+
+// }
+
+
+
+
+
+
