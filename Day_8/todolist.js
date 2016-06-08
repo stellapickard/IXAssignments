2caf6d3b7debd3ebe4102c9a938b5d4618870015
Stella <stella@pickard.co.za>
2016-06-08 14:00:02 +0200

    Day 8 Morning Submission

---
+++ Day_8/todolist.js
@@ -0,0 +1,21 @@
+
+
+// $(function(){
+//     $('#todo-input').val('');
+// });
+
+$(document).ready(function(){
+	$("#todo-input").keyup(function(e) { 
+		var code = e.keyCode;
+		if(code===13){
+			addToList($("#todo-input").val());
+			$("#todo-input").val("");
+		}
+	});
+});
+
+function addToList(value) {
+	console.log(value);
+	var liElem = $("<li></li>").html(value);
+	$("#list").append(liElem);
+};
