cbed02618e1f9e812db7a115b6b3b1063cfa7316
Stella <stella@pickard.co.za>
2016-06-15 12:58:35 +0200

    Day 13 Morning Telephone Book subsmission

---
+++ Day_13/angular.js
@@ -0,0 +1,47 @@
+var app = angular.module("addressBookApp",[]);
+
+app.controller("mainCtrl", function($scope){
+	$scope.newName=" ";
+	$scope.newNumber=" ";
+	$scope.person=[];
+
+
+$scope.submitItems = function(){
+
+// Name validation
+// console.log($scope.person);
+
+	if ($scope.person.newName === " "){
+      alert("Oops, you forgot something!")
+    }; 
+
+// Phone number validation
+
+var numsplit = $scope.newNumber.split("-")
+	if (numsplit.length !== 3){
+		alert("Is the number you entered correct?");
+	};
+	console.log(numsplit);
+
+	var firstNum = numsplit[0];
+	if ((firstNum.length !== 3) || (firstNum === NaN)){
+		alert("Is the number you entered correct?");
+	};
+
+	var midNum = numsplit[1];
+	if ((midNum.length !== 3) || (midNum === NaN)){
+		alert("Is the number you entered correct?");
+	};
+
+	var lastNum = numsplit[2];
+	if ((lastNum.length !== 4) || (lastNum === NaN)){
+		alert("Is the number you entered correct?");
+	};
+
+	$scope.person.push($scope.newName + $scope.newNumber);
+	// console.log($scope.person);    
+
+// }
+};
+// ______ pre final closing 
+});
